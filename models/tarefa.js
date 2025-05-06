6b80b1e2c47575be5888710f632f6ac11db5da4f
Miguel Almeida <maav4907@gmail.com>
2025-05-06 16:46:15 -0300

    versão 2

--- models/tarefa.js
+++ models/tarefa.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 const schemaTarefa = new mongoose.Schema({
-    descricao: {
-        required: true,
-        type: String
-    },
-    statusRealizada: {
-        required: true,
-        type: Boolean
-    },
+ descricao: {
+ required: true,
+ type: String
+ },
+ statusRealizada: {
+ required: true,
+ type: Boolean
+ },
 },
-    {
-        versionKey: false
-    }
+ {
+ versionKey: false
+ }
 )
 module.exports = mongoose.model('Tarefa', schemaTarefa)
