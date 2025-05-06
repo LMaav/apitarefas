f090af9993f2e379b659d6aa19b9f9be78641b71
Miguel Almeida <maav4907@gmail.com>
2025-05-06 16:56:57 -0300

    versão 3

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const porta = process.env.PORT || 3000;
-const conexao = require("./banco-de-dados");
-const Tarefa = require("./modelos/tarefa");
-
+app.use((req, res, next) => {
+ res.setHeader("Access-Control-Allow-Origin", "*");
+ res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, PATCH, DELETE');
+ res.header(
+ "Access-Control-Allow-Headers",
+ "Origin, X-Requested-With, Content-Type, Accept"
+ );
+ next();
+});
 app.use(express.json());
-
-// 🔧 ADICIONE ESTA ROTA:
-app.get("/", (req, res) => {
-  res.send("API de Tarefas está funcionando!");
-});
-
-// Sua rota POST existente:
-app.post("/tarefas", async (req, res) => {
-  const { titulo, descricao } = req.body;
-  const tarefa = new Tarefa({ titulo, descricao });
-
-  await tarefa.save();
-
-  res.status(201).json({ mensagem: "Tarefa criada com sucesso!" });
-});
-
-app.listen(porta, () => {
-  console.log(`Servidor rodando na porta ${porta}`);
-});
-
+const PORT = process.env.PORT || 3000;
+const routes = require('./routes/routes');
+app.use('/api', routes);
+app.listen(PORT, () => {
+ console.log(`Server Started at ${PORT}`)
+})
 // Obtendo os parametros passados pela linha de comando
 var userArgs = process.argv.slice(2);
 var mongoURL = userArgs[0];
